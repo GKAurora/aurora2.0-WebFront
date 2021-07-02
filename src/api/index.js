31b42910be1ba56df5b8cb8a9919d2bfd697591b
farmer <farmer.notscientific@gmail.com>
2021-07-02 13:55:41 +0800

    docs 添加服务器状态api

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,13 @@
 import auth from './auth'
 import sdn from './sdn'
+import server from './server'
 
 const API = {
   server: 'http://c4.farmer233.top:6767',
   // server: 'http://127.0.0.1:5000',
   tokenPrefix: 'Bearer ',
   auth: auth,
-  sdn: sdn
+  sdn: sdn,
+  server: server
 }
 export default API
--- src/api/server.js
+++ src/api/server.js
@@ -30,3 +30,5 @@ const server = {
     }
 
 }
+
+export default server
