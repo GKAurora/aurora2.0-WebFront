64158fd127bf98f7c0682db6f5130564fb135a2a
szz134679 <suzezhi@qq.com>
2021-07-03 04:16:21 +0800

    日志分析完成，只能查看7天的日志

--- src/views/log/UserLog.vue
+++ src/views/log/UserLog.vue
@@ -2,11 +2,22 @@
 <div>
   <el-card>
     <div class='block_1'>
-        <!-- <div class='block_1_1'><el-link class='el-icon-open' style='margin-right: 5px;' @click='hid_show()'></el-link>
-          <span>关联失败：2 认证失败：0 DHCP失败：0</span>
-        </div> -->
         <div class='block_1_1'>
           <img :src='views' id='img' style='width:25px; height: 15px; margin-right: 15px;' @click='hid_show()'><span>查看日志</span>
+          <div :data="SiteList" class="SiteList">
+            <el-radio-group
+              v-model="radio"
+              v-for="(site, index) in SiteList"
+              :key="site.id"
+              size="medium"
+            >
+              <el-radio-button
+                :label="site.name"
+                :key="site.id"
+                @click.native="getSer(site.id, index)"
+              ></el-radio-button>
+            </el-radio-group>
+          </div>
         </div>
         <div class='block_1_2' v-show='hidden_show'>
           <el-divider></el-divider>
@@ -16,26 +27,34 @@
     border
     style='width: 100%'>
     <el-table-column
-      prop='date'
+      prop='timestamp'
       label='时间'
       width='180'>
     </el-table-column>
     <el-table-column
-      prop='name'
-      label='类型'
+      prop='accessTotalNum'
+      label='关联总数'
       width='180'>
     </el-table-column>
     <el-table-column
-      prop='address'
-      label='AP名称'>
+      prop='associateSuccNum'
+      label='关联成功数'>
     </el-table-column>
     <el-table-column
-      prop='address'
-      label='SSID'>
+      prop='associateFailNum'
+      label='关联失败数'>
     </el-table-column>
     <el-table-column
-      prop='address'
-      label='原因'>
+      prop='authSuccNum'
+      label='认证通过数'>
+    </el-table-column>
+    <el-table-column
+      prop='authFailNum'
+      label='认证失败数'>
+    </el-table-column>
+    <el-table-column
+      prop='dhcpSuccNum'
+      label='DHCP成功数'>
     </el-table-column>
           </el-table>
         </div>
@@ -46,64 +65,99 @@
 <script>
 import open from '../../assets/img/about/open.png'
 import close from '../../assets/img/about/close.png'
+import API from '../../api'
 export default {
   data () {
     return {
       hidden_show: false,
       views: close,
-      tableData: [{
-        date: '2016-05-03',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-02',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-02',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-04',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-01',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-08',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-06',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-07',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-06',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-06',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-06',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-06',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }
-      ]
+      name: '',
+      index: 2,
+      // 地区选择
+      radio: 'Shenzhen',
+      // 获取站点信息携带的参数
+      SitequeryInfo: {
+        queryInfoSite_id: '/'
+      },
+      queryInfo: {
+        acc_type: 0,
+        end_time: Date.now(), // 结束时间
+        level: 1,
+        site_id: '857b706e-67d9-49c0-b3cd-4bd1e6963c07', // 站点id
+        start_time: Date.now() - 1000 * 60 * 60 * 24 * parseInt(7) // 前七天的时间
+      },
+      SiteList: [],
+      tableData: []
     }
   },
+  mounted () {
+    this.getSitesMessage()
+    this.getErr()
+  },
   methods: {
+    async getSitesMessage () {
+      try {
+        const conf = API.sdn.getSitesMessage(this.queryInfoSite_id)
+        const data = await this.$axios(conf)
+        if (data.data.code === 200) {
+          this.SiteList = data.data.data
+          console.log(data.data.data)
+          return true
+        }
+        return false
+      } catch (error) {
+        this.$message({
+          message: '获取站点信息失败',
+          type: 'error'
+        })
+      }
+    },
+    async getErr () {
+      try {
+        const conf1 = API.sdn.getErr(
+          this.queryInfo.acc_type,
+          this.queryInfo.end_time,
+          this.queryInfo.start_time,
+          this.queryInfo.site_id,
+          this.queryInfo.level
+        )
+        const data1 = await this.$axios(conf1)
+        // console.log(data1)
+        if (data1.data.code === 200) {
+          this.tableData = data1.data.data
+          for (let i = 0; i < this.tableData.length; i++) {
+            let time = this.tableData[i].timestamp
+            let data_time = new Date(parseInt(time)).toLocaleString().replace(/:\d{1,2}$/, ' ')
+            this.tableData[i].timestamp = data_time
+          }
+          return true
+        }
+        return false
+      } catch (error) {
+        this.$message({
+          message: '获取信息失败',
+          type: 'error'
+        })
+        console.log(error)
+      }
+    },
+    getSer (site_id, index) {
+      // 记录当前选中状态
+      this.index = index
+      this.queryInfo.site_id = site_id
+      var flag = this.getErr()
+      if (flag) {
+        this.$message({
+          message: '查询成功',
+          type: 'success'
+        })
+      } else {
+        this.$message({
+          message: '暂无数据',
+          type: 'error'
+        })
+      }
+    },
     hid_show () {
       if (this.views == close) {
         this.views = open
@@ -117,8 +171,11 @@ export default {
 </script>
 <style lang='scss' scoped>
 .block_1{
-  box-sizing: border-box;
-  box-shadow: 10px 10px 5px #bebdbd;
-  border-radius: .5em;
+  // box-sizing: border-box;
+  // box-shadow: 10px 10px 5px #bebdbd;
+  // border-radius: .5em;
+}
+.SiteList {
+  float: right;
 }
 </style>
