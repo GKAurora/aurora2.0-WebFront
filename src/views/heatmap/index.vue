f7f8e6ee5708e97e2814230676b1f89560465368
szz134679 <suzezhi@qq.com>
2021-07-03 04:15:00 +0800

    热力图已连接接口，但有切换页面时没有渲染，只有窗体改变时有渲染，还要美化，过于简单了

--- src/views/heatmap/index.vue
+++ src/views/heatmap/index.vue
@@ -10,7 +10,7 @@
 
 <script>
 import Heatmap from 'heatmap.js'
-import $ from 'jquery'
+import API from '../../api'
 export default {
   data () {
     return {
@@ -26,22 +26,31 @@ export default {
       this.cavHeight = document.getElementById('heatmap').clientHeight
       // console.log(this.cavWidth, this.cavHeight)
     },
-    // 初始化获取数据（模拟的展示数据）
-    initValue () {
-      var len = 30
-      while (len--) {
-        var val = Math.floor(Math.random() * 100)
-        // max = Math.max(max, val)
-        var point = {
-          // x: Math.floor(Math.random() * 1118),
-          // y: Math.floor(Math.random() * 532),
-          // x: Math.floor(288 * 800 / 1681),
-          // y: Math.floor(108 * 800 / 1681),
-          x: Math.floor(288 * 901 / 1900),
-          y: Math.floor(108 * 901 / 1900),
-          value: val
+    // 初始化获取数据
+    async initValue () {
+      let R = 901 / 1900 // 宽高比例
+      try {
+        const conf = API.sdn.heatmap()
+        const data1 = await this.$axios(conf)
+        let datas = data1.data.data
+        console.log(datas)
+        let x
+        let y
+        let val
+        for (let i = 0; i < datas.length; i++) {
+          val = datas[i].count
+          x = datas[i].x * R
+          y = datas[i].y * R
+          // console.log(val,x,y);
+          var point = {
+            x: x,
+            y: y,
+            value: val
+          }
+          this.points.push(point)
         }
-        this.points.push(point)
+      } catch (error) {
+        console.log(error)
       }
     },
     // 初始化热力图
@@ -51,27 +60,18 @@ export default {
       var heatmapInstance = Heatmap.create({
         container: document.getElementById('heatmap'),
         gradient: {// 渐变颜色
-          '0': 'rgba(255,255,255,1)', // value为0的颜色
-          '0.3': '#82ff6d', // value为500的颜色
-          '0.8': '#f3ff6d', // value为1000的颜色
-          '1': '#ff6d6d' // value为2000的颜色
+          '0': 'rgba(255,255,255,1)',
+          '0.3': '#82ff6d',
+          '0.8': '#f3ff6d',
+          '1': '#ff6d6d'
         },
-        // radius: 10,
+        radius: 15,
         maxOpacity: 1,
         minOpacity: 0,
-        blur: 0.75
-        // radius:30, //半径
-        // opacity:0.5, //透明
-        // maxOpacity, //热图中最大值具有的最大不透明度
-        // minOpacity, //热图中最小值的最小不透明度
-        // onExtremaChange, //传递回调以接收极值更改更新
-        // blur, //将应用于所有数据点的模糊因子。模糊因子越高，渐变将越平滑
-        // xField, //数据点中x坐标的属性名称
-        // yField, //数据点中y坐标的属性名称
-        // valueField //数据点中y坐标的属性名称
+        blur: 1
       })
       var data = {
-        max: 100, // 数据对比(max和min进行数据对比，从而展示热力颜色)
+        max: 50, // 数据对比(max和min进行数据对比，从而展示热力颜色)
         min: 0,
         data: self.points
 
@@ -92,6 +92,7 @@ export default {
   },
   mounted () {
     this.getRectValue()
+    this.initValue()
     this.initHotPic()
     // this.progressDom = document.getElementsByClassName('progress_list')
     var _this = this
@@ -113,7 +114,7 @@ export default {
           }
           _this.initHotPic()
         }
-        console.log('宽=' + curVal, oldVal)
+        // console.log('宽=' + curVal, oldVal)
       }
     },
     cavHeight: {
@@ -123,11 +124,11 @@ export default {
         if (oldVal != 0 && oldVal != null) {
           for (var i = 0; i < this.points.length; i++) {
             this.points[i].y = Number((this.points[i].y * curVal / oldVal).toFixed(0))
-            console.log(this.points[i].y)
+            // console.log(this.points[i].y)
           }
           _this.initHotPic()
         }
-        console.log('高=' + curVal, oldVal)
+        // console.log('高=' + curVal, oldVal)
       }
     }
   }
