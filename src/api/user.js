ad822aa94215edfed198a9839b043d0be7cd9eea
xiaowu0203 <2420674058@qq.com>
2021-07-03 00:09:21 +0800

    用户列表页面完成渲染，但仍需优化

--- src/api/user.js
+++ src/api/user.js
@@ -1,75 +1,6 @@
 import staticRouter from '@/router/staticRouter'
 import axios from '../axios.config'
 
-<<<<<<< HEAD
-// export const requestUserInfo = () => {
-//   return request('/api/user/get_user_info', {}, { 'method': 'get' }).then(res => {
-//     res['permissions'] = [
-//       { name: 'index' },
-//       { name: 'Map' },
-//       { name: 'Map1' },
-//       { name: 'user' },
-//       { name: 'test1' },
-//       { name: 'charts' },
-//       { name: 'health' },
-//       { name: 'log' },
-//       { name: 'account' },
-//       { name: 'errorDemo403' },
-//       { name: 'errorDemo404' },
-//       { name: 'errorDemo500' },
-//       { name: 'drag' },
-//       { name: 'userPassword' },
-//       { name: 'userPermissions' },
-//       { name: 'about' }
-//     ]
-//     console.log(res)
-//     console.log()
-//     // 过滤菜单
-//     const filterUserMenu = function (menus, accessMenu) {
-//       menus.forEach(function (m) {
-//         if (m.noMenu) {
-//           return
-//         }
-//         if (m.children) {
-//           let subMenu = []
-//           filterUserMenu(m.children, subMenu)
-//           if (subMenu.length > 0) {
-//             let _aMenu = Object.assign({}, m)
-//             _aMenu.children = subMenu
-//             accessMenu.push(_aMenu)
-//           }
-//         } else {
-//           res.permissions.some(p => p.name === m.name) && accessMenu.push(m)
-//         }
-//       })
-//     }
-//     let accessMenu = []
-//     let menus = []
-//     staticRouter.forEach(r => {
-//       menus = r.menu ? menus.concat(r.children) : menus
-//     })
-//     filterUserMenu(menus, accessMenu)
-//     res.accessMenu = accessMenu
-//     return res
-//   })
-// }
-
-export const requestLogout = params => {
-  return request('/api/user/logout', params)
-}
-
-export const requestChangePassword = params => {
-  return request('/api/user/changePassword', params)
-}
-
-export const requestUserQuery = params => {
-  return request('/api/user/query', params)
-}
-
-export const requestPermissionsQuery = params => {
-  return request('/api/user/permissions', params)
-}
-=======
 export const requestUserInfo = () => {
   return axios({ 'method': 'get', 'url': '/user/get_user_info' }).then((res) => {
     res['permissions'] = [
@@ -120,4 +51,3 @@ export const requestUserInfo = () => {
     return res
   })
 }
->>>>>>> ce1123e3d17b973d123f72b2042794bcbb1d3979
--- src/views/user/UserList.vue
+++ src/views/user/UserList.vue
@@ -4,18 +4,24 @@
     <el-card>
       <!--搜索与添加区域-->
       <el-row>
-        <el-col :span="6">
-          <div>
-            <el-radio-group v-model="radio" size="medium">
-              <el-radio-button label="深圳"></el-radio-button>
-              <el-radio-button label="南京"></el-radio-button>
-              <el-radio-button label="上海"></el-radio-button>
-              <el-radio-button label="苏州"></el-radio-button>
+        <el-col :span="7">
+          <div :data="SiteList">
+            <el-radio-group
+              v-model="radio"
+              v-for="(site, index) in SiteList"
+              :key="site.id"
+              size="medium"
+            >
+              <el-radio-button
+                :label="site.name"
+                :key="site.id"
+                @click.native="getSer(site.id, index)"
+              ></el-radio-button>
             </el-radio-group>
           </div>
         </el-col>
 
-        <el-col :span="5">
+        <el-col :span="4">
           <div class="block">
             <el-date-picker
               v-model="value1"
@@ -46,19 +52,26 @@
           </div>
         </el-col>
         <el-col :span="3">
-          <el-button type="primary">查询</el-button>
-        </el-col>
-        <el-col :span="3">
           <el-dropdown>
             <el-button type="primary">
               用户类型<i class="el-icon-arrow-down el-icon--right"></i>
             </el-button>
             <el-dropdown-menu slot="dropdown">
-              <el-dropdown-item>所有用户</el-dropdown-item>
-              <el-dropdown-item>有线用户</el-dropdown-item>
-              <el-dropdown-item>无线用户</el-dropdown-item>
+              <el-dropdown-item @click.native="getUser()"
+                >所有用户</el-dropdown-item
+              >
+              <el-dropdown-item @click.native="getNull()"
+                >有线用户</el-dropdown-item
+              >
+              <el-dropdown-item @click.native="getUser()"
+                >无线用户</el-dropdown-item
+              >
             </el-dropdown-menu>
           </el-dropdown>
+        </el-col>
+        <el-col :span="3">
+          <el-button type="primary" @click="editDialogVisible = true"
+            >详细信息</el-button>
         </el-col>
       </el-row>
       <br />
@@ -82,25 +95,20 @@
           align="center"
         ></el-table-column>
         <el-table-column
-          label="所属区域"
-          prop="reGion"
+          label="设备厂商"
+          prop="vendor"
           align="center"
         ></el-table-column>
         <el-table-column
-          label="接入类型"
-          prop="accessType"
+          label="接入总次数"
+          prop="joinTotalTimes"
           align="center"
-        ></el-table-column>
-        <el-table-column label="查看详情" align="center">
+        >
+        </el-table-column>
+        <el-table-column label="接入类型" prop="accessType" align="center">
           <template slot-scope="scope">
-            <el-button
-              type="primary"
-              size="small"
-              round
-              @click="editDialogVisible = true"
-            >
-              详细信息
-            </el-button>
+            <span v-if="scope.row.accessType === 1">无线用户</span>
+            <span v-else>有线用户</span>
           </template>
         </el-table-column>
       </el-table>
@@ -122,132 +130,118 @@
         width="50%"
       >
         <el-card class="box-card">
-          <div style="font-size: 16px; padding: 12px">
+          <div
+            style="font-size: 16px; padding: 12px"
+            :data="UserList"
+            v-for="(name, index) in UserList"
+            :key="name.userMac"
+          >
             <el-row>
-              <el-col :span="8"
+              <el-col :span="7"
                 ><div class="grid-content bg-purple">
-                  <span>用户名：10</span>
+                  用户名：<span v-text="name.userName">10</span>
                 </div></el-col
               >
-              <el-col :span="8"
+              <el-col :span="9"
                 ><div class="grid-content bg-purple-light">
-                  <span>用户 MAC：12</span>
+                  用户 MAC：<span v-text="name.userMac">12</span>
                 </div></el-col
               >
-              <el-col :span="8"
+              <el-col :span="4"
                 ><div class="grid-content bg-purple">
-                  <span>VIP用户：12</span>
+                  VIP用户：<span v-text="name.vipType == 0 ? '否' : '是'"
+                    >12</span
+                  >
                 </div></el-col
               >
             </el-row>
             <br />
             <el-row>
-              <el-col :span="8"
+              <el-col :span="16" style="text-algin: center"
                 ><div class="grid-content bg-purple">
-                  <span>设备厂商：12</span>
+                  设备厂商：<span v-text="name.vendor">12</span>
                 </div></el-col
               >
-              <el-col :span="8"
-                ><div class="grid-content bg-purple-light">
-                  <span>AP名称：12</span>
-                </div></el-col
-              >
-              <el-col :span="8"
+              <el-col :span="7"
                 ><div class="grid-content bg-purple">
-                  <span>AP MAC：12</span>
+                  接入时间：<span v-text="name.accTime">12</span>
                 </div></el-col
               >
             </el-row>
             <br />
             <el-row>
-              <el-col :span="8"
+              <el-col :span="7"
                 ><div class="grid-content bg-purple">
-                  <span>总接入次数：12</span>
+                  总接入次数：<span v-text="name.joinTotalTimes">12</span>
+                </div></el-col
+              >
+              <el-col :span="9"
+                ><div class="grid-content bg-purple-light">
+                  接入失败次数：<span v-text="name.joinFailTimes">12</span>
                 </div></el-col
               >
               <el-col :span="8"
                 ><div class="grid-content bg-purple-light">
-                  <span>接入失败次数：12</span>
-                </div></el-col
-              >
-              <el-col :span="8"
-                ><div class="grid-content bg-purple">
-                  <span>接入类型：12</span>
+                  第一次接入时间：<span v-text="name.minAccTime">12</span>
                 </div></el-col
               >
             </el-row>
             <br />
             <el-row>
-              <el-col :span="8"
+              <el-col :span="7"
                 ><div class="grid-content bg-purple">
-                  <span>本次接入时间：12</span>
+                  总体验时长：<span v-text="name.totalTimes">12</span>
                 </div></el-col
               >
-              <el-col :span="8"
+              <el-col :span="9"
                 ><div class="grid-content bg-purple-light">
-                  <span>第一次接入时间：12</span>
+                  最近接入：<span v-text="name.lastJoinRes">12</span>
                 </div></el-col
               >
               <el-col :span="8"
                 ><div class="grid-content bg-purple">
-                  <span>频段：12</span>
+                  平均接入耗时：<span v-text="name.joinCostTimeAvg">12</span>
                 </div></el-col
               >
             </el-row>
             <br />
             <el-row>
-              <el-col :span="8"
+              <el-col :span="7"
                 ><div class="grid-content bg-purple">
-                  <span>总体验时长：12</span>
+                  平均RSSI：<span v-text="name.rssiAvg">12</span>
                 </div></el-col
               >
-              <el-col :span="8"
+              <el-col :span="9"
                 ><div class="grid-content bg-purple-light">
-                  <span>最近接入：12</span>
+                  平均速率：<span v-text="name.rateAvg">12</span>
                 </div></el-col
               >
               <el-col :span="8"
                 ><div class="grid-content bg-purple">
-                  <span>平均接入耗时：12</span>
+                  平均信噪比：<span v-text="name.snrAvg">12</span>
                 </div></el-col
               >
             </el-row>
             <br />
             <el-row>
-              <el-col :span="8"
+              <el-col :span="7"
                 ><div class="grid-content bg-purple">
-                  <span>平均RSSI：12</span>
+                  总流量：<span v-text="name.totalBytes">12</span>
                 </div></el-col
               >
-              <el-col :span="8"
+              <el-col :span="9"
                 ><div class="grid-content bg-purple-light">
-                  <span>平均速率：12</span>
+                  时延：<span v-text="name.latency">12</span>
                 </div></el-col
               >
               <el-col :span="8"
                 ><div class="grid-content bg-purple">
-                  <span>平均信噪比：12</span>
+                  丢包率：<span v-text="name.packetloss">12</span>%
                 </div></el-col
               >
             </el-row>
             <br />
-            <el-row>
-              <el-col :span="8"
-                ><div class="grid-content bg-purple">
-                  <span>总流量：12</span>
-                </div></el-col
-              >
-              <el-col :span="8"
-                ><div class="grid-content bg-purple-light">
-                  <span>时延：12</span>
-                </div></el-col
-              >
-              <el-col :span="8"
-                ><div class="grid-content bg-purple">
-                  <span>丢包率：12</span>
-                </div></el-col
-              >
-            </el-row>
+            <hr />
           </div>
         </el-card>
       </el-dialog>
@@ -259,8 +253,10 @@ import API from "../../api";
 export default {
   data() {
     return {
+      name: "",
+      index: 2,
       //地区选择
-      radio: "深圳",
+      radio: "Shenzhen",
       //时间选择
       value: [
         new Date(
@@ -313,17 +309,17 @@ export default {
       editDialogVisible: false,
       //获取站点信息携带的参数
       SitequeryInfo: {
-        queryInfoSite_id: "857b706e-67d9-49c0-b3cd-4bd1e6963c07",
+        queryInfoSite_id: "/",
       },
       //获取用户参数列表的对象时携带的参数
       queryInfo: {
         level: 1,
         page: 1, //页数
-        page_size: 100, //每一页的数据总数
+        page_size: 10, //每一页的数据总数
         site_id: "857b706e-67d9-49c0-b3cd-4bd1e6963c07", //站点id
       },
       //存储站点信息
-      site: [],
+      SiteList: [],
       //存储用户列表数据
       UserList: [],
       //数据总条数
@@ -331,21 +327,23 @@ export default {
     };
   },
   mounted() {
-    this.getSite();
-    // this.getUserList();
+    this.getSitesMessage();
+    this.getUserList();
   },
   methods: {
-    async getSite() {
+    //获取站点信息
+    async getSitesMessage() {
       try {
-        console.log("1")
-        const conf = API.sdn.getSitesMessage(this.SitequeryInfo.queryInfoSite_id);
-        console.log("2")
-        const data = await this.$axios(conf)
-        console.log(data);
-        if (data.code === 200) {
-          console.log("1")
-          // this.UserList = data.tableData;
+        const conf = API.sdn.getSitesMessage(
+          this.SitequeryInfo.queryInfoSite_id
+        );
+        const data = await this.$axios(conf);
+        if (data.status === 200) {
+          this.SiteList = data.data.data;
+          console.log(data.data.data);
+          return true;
         }
+        return false;
       } catch (error) {
         this.$message({
           message: "获取站点信息失败",
@@ -353,25 +351,23 @@ export default {
         });
       }
     },
+    //获取用户列表数据
     async getUserList() {
       try {
         const conf = API.sdn.getUserList(
           this.queryInfo.level,
+          this.queryInfo.site_id,
           this.queryInfo.page,
-          this.queryInfo.page_size,
-          this.queryInfo.site_id
+          this.queryInfo.page_size
         );
         const data = await this.$axios(conf);
         console.log(data);
-        if (data.code === 200) {
-          //存储数据
-          console.log('123')
-          return
+        if (data.status === 200) {
+          //存储用户列表数据
+          this.UserList = data.data.data.tableData;
+          //存储用户列表列数
+          this.total = data.data.data.totalSize;
         }
-        this.$message({
-          message: "获取用户数据失败",
-          type: "error",
-        });
       } catch (error) {
         this.$message({
           message: "获取用户数据失败",
@@ -379,17 +375,45 @@ export default {
         });
       }
     },
-    //获取用户列表
-    // async getUserList() {
-    //   const { data: res } = await this.$http.get("users", {
-    //     params: this.queryInfo,
-    //   });
-    //   if (res.meta.status !== 200) {
-    //     return this.$message.error("获取用户列表失败！");
-    //   }
-    //   this.UserList = res.data.users;
-    //   this.total = res.data.total;
-    // },
+    //选择不同地区，展示不同用户列表
+    getSer(site_id, index) {
+      //记录当前选中状态
+      this.index = index;
+      this.queryInfo.site_id = site_id;
+      var flag = this.getUserList();
+      if (flag) {
+        this.$message({
+          message: "查询成功",
+          type: "success",
+        });
+      } else {
+        this.$message({
+          message: "暂无数据",
+          type: "error",
+        });
+      }
+    },
+    //全部用户、无线用户()
+    getUser() {
+      //判断当前是否为深圳站点（这种解决方法需要优化）
+      if (this.index == 2) {
+        this.getSer("857b706e-67d9-49c0-b3cd-4bd1e6963c07", 2);
+      } else {
+        this.$message({
+          message: "暂无数据",
+          type: "info",
+        });
+      }
+    },
+    //有线用户
+    getNull() {
+      this.$message({
+        message: "暂无数据",
+        type: "info",
+      });
+      this.UserList = "";
+      this.total = 0;
+    },
     //监听page_size改变的事件
     handleSizeChange(newSize) {
       // console.log(newSize)
