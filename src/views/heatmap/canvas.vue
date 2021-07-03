9e7bcbb91588ba4671107e1991eb4b7263b67f47
szz134679 <suzezhi@qq.com>
2021-07-03 15:09:55 +0800

    已完成接口，站点和用户Mac是固定的

--- src/views/heatmap/canvas.vue
+++ src/views/heatmap/canvas.vue
@@ -1,11 +1,27 @@
 <template>
   <div id='main_1'>
-      <canvas id='myCanvas' style='width:100%; height:auto'></canvas>
+      <canvas id='myCanvas' style='width:100%; height:auto' @click="dialogVisible = true"></canvas>
       <img id='scream' src='../../assets/img/about/拓扑.png' alt='The Scream' style='display: none;'>
+      <el-dialog
+  title="用户轨迹详情"
+  :visible.sync="dialogVisible"
+  width="30%"
+  :before-close="handleClose">
+  <el-table :data="areas">
+    <el-table-column width="150" property="startTime" label="到达时间"></el-table-column>
+    <el-table-column width="150" property="endTime" label="离开时间"></el-table-column>
+    <el-table-column width="100" property="accessAp" label="活动区域"></el-table-column>
+  </el-table>
+  <span slot="footer" class="dialog-footer">
+    <el-button @click="dialogVisible = false">取 消</el-button>
+    <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+  </span>
+</el-dialog>
   </div>
 </template>
 
 <script>
+import API from '../../api'
 let canvas
 let ctx
 let img
@@ -25,21 +41,54 @@ let area_id =
     'area_11': { 'x': '1110', 'y': '180', 'w': '20', 'h': '20' }
   }
   // 遍历后端的用户区域
-let area_id2 = {
-  'area_2': {},
-  'area_3': {},
-  'area_1': {},
-  'area_5': {},
-  'area_9': {}
-}
+let area_id2 = {}
 export default {
   data () {
     return {
       cavWidth: 1118, // 画布默认宽度
-      cavHeight: 559 // 画布默认高度
+      cavHeight: 559, // 画布默认高度
+      dialogVisible: false,
+      areas: [],
+      queryInfo: {
+        level: 1,
+        site_id: '857b706e-67d9-49c0-b3cd-4bd1e6963c07', // 站点id
+        user_mac: '97-ff-d2-49-7f-bd'
+      }
     }
   },
   methods: {
+    async initarea () {
+      try {
+        const conf = API.sdn.getUserRoute(this.queryInfo.level, this.queryInfo.site_id, this.queryInfo.user_mac)
+        const data = await this.$axios(conf)
+        // console.log(data.data.data)
+        if (data.data.code === 200) {
+          this.areas = data.data.data
+          for (let i = 0; i < this.areas.length; i++) {
+          // console.log(areas[i].accessAp)
+            area_id2[this.areas[i].accessAp] = {}
+            let data_time = new Date(parseInt(this.areas[i].startTime)).toLocaleString().replace(/:\d{1,2}$/, ' ')
+            this.areas[i].startTime = data_time
+            let end_time = new Date(parseInt(this.areas[i].endTime)).toLocaleString().replace(/:\d{1,2}$/, ' ')
+            this.areas[i].endTime = end_time
+          }
+        }
+        // console.log(area_id2);
+      } catch (error) {
+        this.$message({
+          message: '获取信息失败',
+          type: 'error'
+        })
+        console.log(error)
+      }
+    },
+    handleClose (done) {
+      this.$confirm('确认关闭？')
+        .then(_ => {
+          done()
+        })
+        .catch(_ => {})
+    },
     initCanvas () {
       console.log('初始化canvas')
       var a_key
@@ -140,9 +189,9 @@ export default {
             ctx.globalCompositeOperation = 'destination-over'
             ctx.strokeStyle = 'rgba(255 ,165, 0, 1.0)'
             ctx.moveTo(oldx, oldy)
-            ctx.lineTo(oldx, oldy-12)
-            ctx.lineTo(x,oldy-12)
-            ctx.lineTo(x,y)
+            ctx.lineTo(oldx, oldy - 12)
+            ctx.lineTo(x, oldy - 12)
+            ctx.lineTo(x, y)
             // ctx.quadraticCurveTo(oldx, oldy - 20, x, y)
             ctx.stroke()
             ctx.closePath()
@@ -162,10 +211,10 @@ export default {
             ctx.beginPath()
             ctx.globalCompositeOperation = 'destination-over'
             ctx.strokeStyle = 'rgba(255 ,165, 0, 1.0)'
-            ctx.moveTo(oldx+5, oldy)
-            ctx.lineTo(oldx+5, oldy-17)
-            ctx.lineTo(x,oldy-17)
-            ctx.lineTo(x,y)
+            ctx.moveTo(oldx + 5, oldy)
+            ctx.lineTo(oldx + 5, oldy - 17)
+            ctx.lineTo(x, oldy - 17)
+            ctx.lineTo(x, y)
             // ctx.arcTo(x,y+30,x+10,y+30,10)
             // ctx.lineTo(x+30,y+30)
             // ctx.arcTo(oldx,y+30,oldx,oldy,10)
@@ -190,12 +239,12 @@ export default {
             ctx.globalCompositeOperation = 'destination-over'
             ctx.strokeStyle = 'rgba(255 ,165, 0, 1.0)'
             ctx.moveTo(oldx, oldy)
-            ctx.lineTo(oldx+50, oldy)
-            ctx.lineTo(oldx+50,oldy+20)
-            ctx.lineTo(oldx+60,oldy+20)
-            ctx.lineTo(oldx+60,oldy+35)
-            ctx.lineTo(x,oldy+35)
-            ctx.lineTo(x,y)
+            ctx.lineTo(oldx + 50, oldy)
+            ctx.lineTo(oldx + 50, oldy + 20)
+            ctx.lineTo(oldx + 60, oldy + 20)
+            ctx.lineTo(oldx + 60, oldy + 35)
+            ctx.lineTo(x, oldy + 35)
+            ctx.lineTo(x, y)
             ctx.stroke()
             ctx.closePath()
             ctx.beginPath()
@@ -214,14 +263,14 @@ export default {
             ctx.beginPath()
             ctx.globalCompositeOperation = 'destination-over'
             ctx.strokeStyle = 'rgba(255 ,165, 0, 1.0)'
-            ctx.moveTo(oldx+5, oldy)
-            ctx.lineTo(oldx+5,oldy-15)
-            ctx.lineTo(x,oldy-15)
+            ctx.moveTo(oldx + 5, oldy)
+            ctx.lineTo(oldx + 5, oldy - 15)
+            ctx.lineTo(x, oldy - 15)
             // ctx.lineTo(oldx+30, oldy)
             // ctx.arcTo(oldx+2,oldy-15,oldx+15,oldy-15,15)
             // ctx.lineTo(oldx+120,oldy-15)
             // ctx.arcTo(oldx+130,oldy-15,oldx+130,oldy-25,10)
-            ctx.lineTo(x,y)
+            ctx.lineTo(x, y)
             ctx.stroke()
             ctx.closePath()
             ctx.beginPath()
@@ -233,34 +282,23 @@ export default {
             ctx.font = '10px Verdana'
             ctx.fillText(sum[i], x - 20, y + 15)
             ctx.closePath()
-          } 
-        }
-        canvas.onclick = function (e) {
-        // 处理canvas的按下鼠标事件
-        //   let keyid2 = {}
-        //   for (const key_id2 in area_id2) {
-        //     keyid2[key_id2] = {}
-        //     // console.log(keyid2[key_id2])
-        //   }
-        //   var key2 = Object.keys(keyid2)
-        //   for (let i=0;i<key2.length;i++) {
-        //     console.log(key2[i] + keyid2[key2[i]])
-        //   }
-        //   alert(keyid2)
-        // }
-          var key2 = Object.keys(area_id2)
-          var sum = []
-          for (let i = 0; i < key2.length; i++) {
-            sum.push(key2[i])
           }
-          let sum_string = sum.join('-->')
-          alert('用户路径：' + sum_string)
         }
+        // canvas.onclick = function (e) {
+        //   var key2 = Object.keys(area_id2)
+        //   var sum = []
+        //   for (let i = 0; i < key2.length; i++) {
+        //     sum.push(key2[i])
+        //   }
+        //   let sum_string = sum.join('-->')
+        //   alert('用户一周的路径：' + sum_string)
+        // }
       }
     }
   },
   mounted () {
     this.initCanvas()
+    this.initarea()
     var _this = this
     window.onresize = () => {
       return (() => {
